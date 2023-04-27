8e6ec8a32e7901ed86d8940f32b934e5a52453e0
inespascoal <130778859+inespascoal@users.noreply.github.com>
2023-04-27 19:33:56 +0100

    Update robot.py

--- robot.py
+++ robot.py
@@ -19,36 +19,33 @@ def forward():
     servo.setServoAngle(8,100)
 
 
-     while True:
-         for i in range(180):
-             servo.setServoAngle(3,120-i/3)
-             #servo.setServoAngle(2,70+i/8) 
-             servo.setServoAngle(9,60+i/3) # para de trás direita faz o mesmo
-                
-             time.sleep(0.01)
-            
-             servo.setServoAngle(12, 120-i/3)
-             #servo.setServoAngle(13, 110-i/4) 
 
-         
-             servo.setServoAngle(6,120-i/3) # a pata frente esquerda anda para a frente
-             
-               servo.setServoAngle(3,60+i/3)
-             #servo.setServoAngle(2,70+i/8) 
-             servo.setServoAngle(9,120-i/3) # para de trás direita faz o mesmo
-                
-             time.sleep(0.01)
-            
-             servo.setServoAngle(12, 60+i/3)
-             #servo.setServoAngle(13, 110-i/4) 
+     for i in range(180):
+         servo.setServoAngle(3,120-i/3)
+         #servo.setServoAngle(2,70+i/8) 
+         servo.setServoAngle(9,60+i/3) # para de trás direita faz o mesmo
 
-         
-             servo.setServoAngle(6,120-i/3) # a pata frente esquerda anda para a frente
+         time.sleep(0.01)
+
+         servo.setServoAngle(12, 120-i/3)
+         #servo.setServoAngle(13, 110-i/4) 
 
 
-         print ("\nEnd of program")
-     except KeyboardInterrupt:
-         print ("\nEnd of program")
+         servo.setServoAngle(6,120-i/3) # a pata frente esquerda anda para a frente
+
+           servo.setServoAngle(3,60+i/3)
+         #servo.setServoAngle(2,70+i/8) 
+         servo.setServoAngle(9,120-i/3) # para de trás direita faz o mesmo
+
+         time.sleep(0.01)
+
+         servo.setServoAngle(12, 60+i/3)
+         #servo.setServoAngle(13, 110-i/4) 
+
+
+         servo.setServoAngle(6,120-i/3) # a pata frente esquerda anda para a frente
+
+
 
     
 
